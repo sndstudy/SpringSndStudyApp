667a299d7d7b20e54fd703b9900f0a4235ea2c74
sndstudy <sanada2054ss@gmail.com>
2017-09-17 21:20:56 +0900

    first commit

---
+++ src/main/java/sndstudy/controller/MainController.java
@@ -0,0 +1,16 @@
+package sndstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value="/")
+	public String test() {
+
+		return "test";
+
+	}
+
+}
